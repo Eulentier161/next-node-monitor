7f2af71b1cb6323fd527fd094faac6e96f3a1763
Eulentier161 <git@eule.wtf>
2025-12-17 23:01:14 +0100

    benis

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ this [`docker-compose.yml`](./docker-compose.yml) will set you up with a running
 ```yml
 services:
   banano-monitor:
-    image: ghcr.io/eulentier161/next-node-monitor:0.1.0
+    image: ghcr.io/eulentier161/next-node-monitor:0.1.1
     restart: unless-stopped
     ports:
       - "127.0.0.1:3000:3000"
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="my-8 px-2 text-center text-sm text-gray-500">
+    <footer className="container mx-auto my-8 px-2 text-center text-sm text-gray-500">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quia quidem hic exercitationem aliquam, omnis
-        eos. Debitis libero accusantium fugiat nostrum sint, ea expedita minus, nesciunt suscipit ex ratione assumenda.
+        Our{" "}
+        <Link className="underline" target="_blank" href="https://banano.cc/">
+          Banano
+        </Link>
+        , which art in{" "}
+        <Link className="underline" target="_blank" href="https://kalium.banano.cc/">
+          Kalium
+        </Link>
+        , Hallowed be thy Seed. Thy send block come. Thy receive block be done in{" "}
+        <Link className="underline" target="_blank" rel="noopener noreferrer" href="https://reddit.com/r/banano/">
+          reddit
+        </Link>
+        , As it is in{" "}
+        <Link className="underline" target="_blank" rel="noopener noreferrer" href="https://chat.banano.cc/">
+          discord
+        </Link>
+        . Give us this day our daily rains. And forgive us our hello dears, As we forgive those that hello dear against
+        us. And lead us not into phishing emails, But deliver us from scammers. For thine is the wallet, The key, and
+        the sook, For ever and ever. Amen.
       </p>
     </footer>
   );
